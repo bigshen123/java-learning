47520e43da44e88bd7726d1a60f38bb3213a4fd5
bayongjie <bayj@koal.com>
2022-08-30 21:52:52 +0800

    fix： cherry-pick test2

--- src/main/java/com/bigshen/learningDemo/utils/CherryPick.java
+++ src/main/java/com/bigshen/learningDemo/utils/CherryPick.java
@@ -9,5 +9,6 @@ public class CherryPick {
     public static void main(String[] args) {
         System.out.println("Cherry-Pick。。。。。。");
         System.out.println("Cherry-Pick 第一次提交");
+        System.out.println("Cherry-Pick 第二次提交");
     }
 }
