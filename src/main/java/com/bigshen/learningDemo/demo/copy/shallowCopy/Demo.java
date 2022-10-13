96f4d5326d7fbd7ff8481abe69eef2d86166a474
bayj <bbigshen@163.com>
2022-10-13 17:56:29 +0800

    fix：add 深浅clone demo

---
+++ src/main/java/com/bigshen/learningDemo/demo/copy/shallowCopy/Demo.java
@@ -0,0 +1,32 @@
+package com.bigshen.learningDemo.demo.copy.shallowCopy;
+
+/**
+ * @author byj
+ * @date 2022/10/13
+ * 浅拷贝（基本类型和String类型互不影响，对象类型相互影响）
+ */
+public class Demo {
+    public static void main(String[] args) {
+        School school1 = new School();
+        school1.age = 1;
+        school1.name = "One";
+        school1.subject = new Subject("Java", 1024);
+        System.out.println("原先的school1：" + school1);
+
+        School school2;
+        try {
+            school2 = (School) school1.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("原先的school2：" + school2);
+
+        school2.age = 2;                  //改变clone后的age
+        school2.name = "Two";             //改变clone后的name
+        school2.subject.name = "New";     //改变clone后的subject对象的name
+        school2.subject.classNum = 1025;  //改变clone后的subject对象的name
+        System.out.println("修改后的school1：" + school1);
+        System.out.println("修改后的school2：" + school2);
+    }
+}
---
+++ src/main/java/com/bigshen/learningDemo/demo/copy/shallowCopy/School.java
@@ -0,0 +1,27 @@
+package com.bigshen.learningDemo.demo.copy.shallowCopy;
+
+/**
+ * @author byj
+ * @date 2022/10/13
+ */
+class School implements Cloneable {
+    int age;                 //基本数据类型
+    String name;             //String引用类型
+    Subject subject;         //类的对象，引用类型
+
+    // 由于 Object 本身没有实现 Cloneable 接口
+    // 所以不重写 clone 方法并且进行调用的话会发生 CloneNotSupportedException 异常。
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+
+    @Override
+    public String toString() {
+        return "School{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", subject=" + subject +
+                '}';
+    }
+}
