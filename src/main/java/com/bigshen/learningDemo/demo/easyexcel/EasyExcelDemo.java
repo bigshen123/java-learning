51d0039c9a18e07da4cb5552cda6d2d1c3bbd9f9
巴永杰 <bbigshen@163.com>
2023-07-04 17:56:20 +0800

    fix：add easyexcel demo

---
+++ src/main/java/com/bigshen/learningDemo/demo/easyexcel/EasyExcelDemo.java
@@ -0,0 +1,35 @@
+package com.bigshen.learningDemo.demo.easyexcel;
+
+import com.alibaba.excel.EasyExcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author byj
+ * @date 2023/7/4
+ *  see https://easyexcel.opensource.alibaba.com/docs/current/
+ */
+public class EasyExcelDemo {
+
+    public static void main(String[] args) {
+        // 准备数据
+        List<User> userList = generateData();
+
+        // 导出Excel文件
+        String fileName = "src/main/resources/file.xlsx";
+        EasyExcel.write(fileName, User.class).sheet("Sheet1").doWrite(userList);
+        System.out.println("Excel导出成功！");
+    }
+
+    private static List<User> generateData() {
+        List<User> userList = new ArrayList<>();
+
+        // 添加示例数据
+        userList.add(new User("Alice", 25, "alice@example.com"));
+        userList.add(new User("Bob", 30, "bob@example.com"));
+        userList.add(new User("Charlie", 28, "charlie@example.com"));
+
+        return userList;
+    }
+}
