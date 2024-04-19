fbde90b5c748009087b311d2c6f798192658966a
巴永杰 <bbigshen@163.com>
2024-04-19 15:58:31 +0800

    feat: 目录迁移

---
+++ src/main/java/com/bigshen/learningDemo/demo/io/IODemo/ReadGif.java
@@ -0,0 +1,34 @@
+package com.bigshen.learningDemo.demo.io.IODemo;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+/**
+ * @author byj
+ * @date 2024/4/19
+ * @Description
+ */
+public class ReadGif {
+    public static void main(String[] args) {
+        try {
+            // 读取GIF文件
+//            Path path = Paths.get("C:\\Users\\Lenovo\\Desktop\\2785.gif_wh860.gif");
+//            byte[] gifBytes = Files.readAllBytes(path);
+//            // 将字节数组编码为Base64字符串
+//            String base64String = Base64.getEncoder().encodeToString(gifBytes);
+//            // 输出Base64字符串
+//            System.out.print(base64String);
+
+            String base64GIT = "R0lGODlhAQABAIABAP///wAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==";
+            byte[] decode = Base64.getDecoder().decode(base64GIT);
+            Files.write(Paths.get("C:\\Users\\Lenovo\\Desktop\\2.gif"),decode);
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
